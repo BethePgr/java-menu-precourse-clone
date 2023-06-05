3ac9221c3bf51f9cfcf23a5af818b816ca8b8f77
SEC <c3392302@naver.com>
2023-06-05 11:52:40 +0900

    feat(coach):먹을 메뉴에 대한 getter, setter 메 서드 추가

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -8,6 +8,7 @@ public class Coach {
 
     private final String name;
     private final List<String> cantEatMenu;
+    private List<String> eatMenu;
 
     public Coach(String name, List<String> cantEatMenu) {
         this.name = name;
@@ -21,5 +22,13 @@ public class Coach {
 
     public List<String> getCantEatMenu() {
         return cantEatMenu;
+    }
+
+    public void setEatMenu(List<String> eatMenu){
+        this.eatMenu = eatMenu;
+    }
+
+    public List<String> getEatMenu(){
+        return eatMenu;
     }
 }
